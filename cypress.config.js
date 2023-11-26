4cd5f40f65c2ea257b199d9806ce3c85cd310467
oliverpedroso <oliver1508@hotmail.com>
2023-11-25 21:20:37 -0300

    Adding mobile viewport and changed cypress version

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,20 @@
-const { defineConfig } = require('cypress')
+
+const { defineConfig } = require('cypress');
+
+// module.exports = defineConfig({
+//   viewportHeight: 880,
+//   viewportWidth: 1280,
+//   projectId: "5ztq8z",
+//   e2e: {
+//     setupNodeEvents(on, config) {},
+//   },
+// });
 
 module.exports = defineConfig({
-  viewportHeight: 880,
-  viewportWidth: 1280,
+  viewportHeight: 860,
+  viewportWidth: 410,
   projectId: "5ztq8z",
   e2e: {
     setupNodeEvents(on, config) {},
   },
-})
+});
