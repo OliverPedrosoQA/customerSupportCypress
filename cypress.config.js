2fd8dc3a4fe9bd350d0d365e406830df15ad511d
OliverPedrosoQA <oliver1508@hotmail.com>
2024-01-02 19:57:46 -0300

    Added config changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,9 @@
-
 const { defineConfig } = require('cypress');
 
 //WEB - This settings can be overwritten by the viewport settings from the package.json file
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  projectId: "5ztq8z",
   e2e: {
     setupNodeEvents(on, config) {},
   },
